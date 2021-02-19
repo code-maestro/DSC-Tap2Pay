5f412986c18bb261b77319788ec17b10b3bb1f3a
mikael <michaelajnew@gmail.com>
2021-02-19 16:44:12 +0300

    First trial of NFC implementation in the MainActivity.java Added the NFC permission and Hardware in manifest.

--- app/src/main/java/com/example/tap2pay/MainActivity.java
+++ app/src/main/java/com/example/tap2pay/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tap2pay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView mView = (TextView) this.findViewById(R.id.check);
+
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+
+        if (nfcAdapter!=null || nfcAdapter.isEnabled()){
+
+            mView.setText("NFC IS AVAILABLE AND ENABLED 🤣🤣🤣 NOTHING KUMANYOKO");
+
+            Toast.makeText(this, "NFC IS AVAILABLE AND ENABLED", Toast.LENGTH_SHORT)
+                    .show();
+        }else{
+            mView.setText("NO NFC, 🤣🤣🤣 NOTHING KUMANYOKO");
+        }
+
     }
 }
