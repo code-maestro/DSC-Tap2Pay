941282142f358d88eceb9df8042033f4d457d47f
mikael <michaelajnew@gmail.com>
2021-02-23 13:20:57 +0300

    Implemented the Activity Life Cycle methods onResume and onPause to handle the detection of the NFC tag
    Signed-off-by: mikael <michaelajnew@gmail.com>

--- app/src/main/java/com/example/tap2pay/MainActivity.java
+++ app/src/main/java/com/example/tap2pay/MainActivity.java
@@ -1,16 +1,10 @@
 package com.example.tap2pay;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.nfc.NfcAdapter;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
 
         mButton.setOnClickListener(v -> {
             startActivity(new Intent(this, TestActivity.class));
+            finish();
         });
 
     }
--- app/src/main/java/com/example/tap2pay/TestActivity.java
+++ app/src/main/java/com/example/tap2pay/TestActivity.java
@@ -1,10 +1,14 @@
 package com.example.tap2pay;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -20,11 +24,45 @@ public class TestActivity extends AppCompatActivity {
 
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
 
-        if (mNfcAdapter!= null && mNfcAdapter.isEnabled()){
+        if (mNfcAdapter != null && mNfcAdapter.isEnabled()) {
             welcome.setText("NFC 😎😎 IS ENABLED AND AVALIABLE");
-        }else{
+        } else {
             welcome.setText("NFC NOT ENABLED 😌😌 NOTHING VIBES JUST ");
         }
 
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+
+        Toast.makeText(this, "NFC INTENT RECEIVED ", Toast.LENGTH_SHORT).show();
+
+        super.onNewIntent(intent);
+    }
+
+    @Override
+    protected void onResume() {
+
+        Intent intent = new Intent(this, TestActivity.class);
+
+        intent.addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING);
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, intent, 0);
+
+        IntentFilter[] intentFilters = new IntentFilter[]{};
+
+        mNfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFilters,
+                null);
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+
+        mNfcAdapter.disableForegroundDispatch(this);
+
+        super.onPause();
     }
 }
